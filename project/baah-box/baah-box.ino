5701f94e382b99f7d6a7a78aea114e5565f2c29e
Frédérique Pinson <frederique.pinson@gmail.com>
2023-01-06 15:02:42 +0100

    started updating code for nrf52 cards

--- project/baah-box/baah-box.ino
+++ project/baah-box/baah-box.ino
@@ -33,6 +33,12 @@ handzDisplay display3dhandz;
 // SDCard
 configSDClass config3dHandz;
 
+// tinyUSB for nrf52
+#if defined(USE_TINYUSB)
+#include <Adafruit_TinyUSB.h> // for Serial
+#endif
+
+
 //*********************************************
 //*
 //*       S E T U P
