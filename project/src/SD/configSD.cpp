683ae29a6e78aabb073d0cd69e768d68e62aea4a
Frédérique Pinson <frederique.pinson@gmail.com>
2025-11-04 21:04:34 +0100

    minor fixes

--- project/src/SD/configSD.cpp
+++ project/src/SD/configSD.cpp
@@ -84,7 +84,7 @@ void configSDClass::loadDefaultValues(void)
 {
   this->btleDeviceName = "Baah Box";
   this->nbMuscleSensor = NB_MUSCLE_SENSOR;
-  //Serial.print("Default values for sensors : ");
+  Serial.print("Default values for sensors : ");
   this->pinAnalogInputTab[0] = 0;
   this->pinAnalogInputTab[1] = 1;
   this->joystickDigitalInputTab[0] = A5;
